db09804c4f3d267d647580cad2476a75c7adf2e4
Yeint Herp <yeint_herp@proton.me>
2026-03-17 19:34:31 +0100

    feat: Debug printer initialization in early Hal init and refactoring of arch macros

--- supervisor/source/hal/Hal.c
+++ supervisor/source/hal/Hal.c
@@ -1,3 +1,13 @@
+#include <debug/DbgPrint.h>
 #include <hal/Hal.h>
 
-void Hal_InitializeEarly() {}
+void Hal_InitializeEarly()
+{
+    Dbg_RegisterSinker(g_RingBufSinker);
+    Dbg_Log(TRACE, "hal", "early ring buffer sinker ready");
+
+    if (kArch == x86_64)
+        Dbg_RegisterSinker(g_e9Sinker);
+
+    Dbg_Log(INFO, "hal", "early initialization done");
+}
--- supervisor/source/hal/Hal.h
+++ supervisor/source/hal/Hal.h
@@ -1,6 +1,8 @@
 #ifndef SUPERVISOR_HAL_HAL_H
 #define SUPERVISOR_HAL_HAL_H
 
+#define x86_64 1
+
 [[noreturn]] void Hal_HaltCatchFire(void);
 
 void Hal_InitializeEarly(void);
