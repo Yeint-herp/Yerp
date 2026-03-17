b97e5a6eedfdccb8a607758f8095bc574d856c42
Yeint Herp <yeint_herp@proton.me>
2026-03-17 18:46:38 +0100

    refactor: Rename s* to i* signed types and add macro helpers

--- supervisor/const.h
+++ supervisor/const.h
@@ -6,10 +6,10 @@ typedef unsigned short u16;
 typedef unsigned int   u32;
 typedef unsigned long  u64;
 
-typedef signed char	 i8;
+typedef signed char  i8;
 typedef signed short i16;
-typedef signed int	 i32;
-typedef signed long	 i64;
+typedef signed int   i32;
+typedef signed long  i64;
 
 typedef u64 usize;
 typedef i64 isize;
@@ -20,6 +20,10 @@ typedef isize iptr;
 typedef uptr paddr_t;
 typedef uptr vaddr_t;
 
-#define unreachable() __builtin_unreachable()
+#define unreachable()  __builtin_unreachable()
+#define ArraySize(arr) sizeof(arr) / sizeof(*arr)
+
+#define Bit_FirstSetIndex(x)   __builtin_ctz(x)
+#define Bit_FirstClearIndex(x) Bit_FirstSetIndex(~x)
 
 #endif /* SUPERVISOR_CONST_H */
