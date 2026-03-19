2cecc9e5c1366e4ac062ebae29d927ee343777bf
Yeint Herp <yeint_herp@proton.me>
2026-03-19 18:45:01 +0100

    feat: Kernel memory map support

---
+++ supervisor/source/mm/MemMap.c
@@ -0,0 +1,50 @@
+#define DBG_MODULE "Mm"
+
+#include <debug/DbgPrint.h>
+#include <mm/MemMap.h>
+
+const char *Mm_GetRegionTypeString(Mm_RegionType type)
+{
+    switch (type)
+    {
+        case MEM_TYPE_USABLE:
+            return "USABLE";
+        case MEM_TYPE_RESERVED:
+            return "RESERVED";
+        case MEM_TYPE_ACPI_RECLAIMABLE:
+            return "ACPI_RECLAIMABLE";
+        case MEM_TYPE_ACPI_NVS:
+            return "ACPI_NVS";
+        case MEM_TYPE_BAD_MEMORY:
+            return "BAD_MEMORY";
+        case MEM_TYPE_BOOTLOADER_RECLAIMABLE:
+            return "BOOTLOADER_RECLAIMABLE";
+        case MEM_TYPE_KERNEL_AND_MODULES:
+            return "KERNEL_AND_MODULES";
+        case MEM_TYPE_FRAMEBUFFER:
+            return "FRAMEBUFFER";
+        case MEM_TYPE_EARLY_ALLOCATED:
+            return "EARLY_ALLOCATED";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+void Mm_DumpMemMap(const Mm_KernelMemMap *map)
+{
+    if (!map || !map->Regions)
+    {
+        Log(WARN, "memory map is null");
+        return;
+    }
+
+    for (usize i = 0; i < map->Count; i++)
+    {
+        const Mm_MemRegion *reg = &map->Regions[i];
+
+        u64 top = reg->Base + reg->Length;
+
+        Log(INFO, "[%02zu] %#018llx - %#018llx | Size: %-8zZ | %-25s", i, reg->Base, top, reg->Length,
+            Mm_GetRegionTypeString(reg->Type));
+    }
+}
---
+++ supervisor/source/mm/MemMap.h
@@ -0,0 +1,36 @@
+#ifndef SUPERVISOR_MM_MEMMAP_H
+#define SUPERVISOR_MM_MEMMAP_H
+
+typedef enum
+{
+    MEM_TYPE_USABLE,
+    MEM_TYPE_RESERVED,
+    MEM_TYPE_ACPI_RECLAIMABLE,
+    MEM_TYPE_ACPI_NVS,
+    MEM_TYPE_BAD_MEMORY,
+    MEM_TYPE_BOOTLOADER_RECLAIMABLE,
+    MEM_TYPE_KERNEL_AND_MODULES,
+    MEM_TYPE_FRAMEBUFFER,
+    MEM_TYPE_EARLY_ALLOCATED
+} Mm_RegionType;
+
+typedef struct
+{
+    u64 Base;
+    u64 Length;
+
+    Mm_RegionType Type;
+} Mm_MemRegion;
+
+typedef struct
+{
+    Mm_MemRegion *Regions;
+
+    usize Count;
+    usize Capacity;
+} Mm_KernelMemMap;
+
+const char *Mm_GetRegionTypeString(Mm_RegionType type);
+void        Mm_DumpMemMap(const Mm_KernelMemMap *map);
+
+#endif /* SUPERVISOR_MM_MEMMAP_H */
