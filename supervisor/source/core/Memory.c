23c68abe8de2093c2a14fb3797bfd86673fbad3f
Yeint Herp <yeint_herp@proton.me>
2026-03-26 14:14:57 +0100

    feat: Add String helpers into core/Memory

--- supervisor/source/core/Memory.c
+++ supervisor/source/core/Memory.c
@@ -1,4 +1,5 @@
 #include <core/Memory.h>
+#include <mm/Pool.h>
 
 void *Core_CopyMemory(void *dst, const void *src, usize size)
 {
@@ -109,3 +110,124 @@ void *memcpy(void *dst, const void *src, usize size) __attribute__((alias("Core_
 void *memmove(void *dst, const void *src, usize size) __attribute__((alias("Core_MoveMemory")));
 void *memset(void *dst, int value, usize size) __attribute__((alias("Core_FillMemory")));
 int   memcmp(const void *a, const void *b, usize size) __attribute__((alias("Core_CompareMemory")));
+
+usize Core_StringLength(const char *str)
+{
+    const char *p = str;
+
+    while (*p)
+        p++;
+
+    return p - str;
+}
+
+i32 Core_CompareString(const char *a, const char *b)
+{
+    while (*a && *a == *b)
+    {
+        a++;
+        b++;
+    }
+
+    return *a - *b;
+}
+
+i32 Core_CompareStringN(const char *a, const char *b, usize maxLen)
+{
+    if (maxLen == 0)
+        return 0;
+
+    while (maxLen-- > 1 && *a && *a == *b)
+    {
+        a++;
+        b++;
+    }
+
+    return *a - *b;
+}
+
+char *Core_CopyString(char *dst, const char *src)
+{
+    char *d = dst;
+
+    while ((*d++ = *src++))
+        ;
+
+    return dst;
+}
+
+char *Core_CopyStringN(char *dst, const char *src, usize maxLen)
+{
+    char *d = dst;
+
+    if (maxLen == 0)
+        return dst;
+
+    while (maxLen-- > 1 && *src)
+        *d++ = *src++;
+
+    *d = '\0';
+
+    return dst;
+}
+
+const char *Core_FindChar(const char *str, char c)
+{
+    while (*str)
+    {
+        if (*str == c)
+            return str;
+
+        str++;
+    }
+
+    return c == '\0' ? str : nullptr;
+}
+
+const char *Core_FindCharReverse(const char *str, char c)
+{
+    const char *last = nullptr;
+
+    while (*str)
+    {
+        if (*str == c)
+            last = str;
+
+        str++;
+    }
+
+    return c == '\0' ? str : last;
+}
+
+const char *Core_FindSubstring(const char *haystack, const char *needle)
+{
+    if (*needle == '\0')
+        return haystack;
+
+    usize needleLen = Core_StringLength(needle);
+
+    while (*haystack)
+    {
+        if (*haystack == *needle)
+            if (Core_CompareStringN(haystack, needle, needleLen) == 0)
+                return haystack;
+
+        haystack++;
+    }
+
+    return nullptr;
+}
+
+char *Core_DuplicateString(const char *str, u32 tag)
+{
+    if (!str)
+        return nullptr;
+
+    usize len = Core_StringLength(str) + 1;
+    char *dup = Ex_Allocate(len, tag);
+
+    if (dup)
+        Core_CopyMemory(dup, str, len);
+
+    return dup;
+}
--- supervisor/source/core/Memory.h
+++ supervisor/source/core/Memory.h
@@ -9,4 +9,15 @@ i32 Core_CompareMemory(const void *a, const void *b, usize size);
 
 void *Core_ZeroMemory(void *dst, usize size);
 
+usize       Core_StringLength(const char *str);
+i32         Core_CompareString(const char *a, const char *b);
+i32         Core_CompareStringN(const char *a, const char *b, usize maxLen);
+char       *Core_CopyString(char *dst, const char *src);
+char       *Core_CopyStringN(char *dst, const char *src, usize maxLen);
+const char *Core_FindChar(const char *str, char c);
+const char *Core_FindCharReverse(const char *str, char c);
+const char *Core_FindSubstring(const char *haystack, const char *needle);
+
+char *Core_DuplicateString(const char *str, u32 tag);
+
 #endif /* SUPERVISOR_CORE_MEMORY_H */
