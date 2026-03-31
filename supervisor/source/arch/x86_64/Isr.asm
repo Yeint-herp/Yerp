01a22988ef9839ccedcca156e5c3306efb2e2150
Yeint Herp <yeint_herp@proton.me>
2026-03-31 20:39:42 +0200

    feat: cfi and other dwarf debug info in assembler code

--- supervisor/source/arch/x86_64/Isr.asm
+++ supervisor/source/arch/x86_64/Isr.asm
@@ -41,33 +41,54 @@
 
 .equ TF_SIZE, 240
 
-.section .text
+.section .text, "ax", @progbits
 .p2align 4, 0x90
 
 .globl IsrCommonInterruptStub
+.type IsrCommonInterruptStub, @function
 IsrCommonInterruptStub:
+    .cfi_startproc simple
+    .cfi_signal_frame
+
+    .cfi_def_cfa rsp, 56
     test qword ptr [rsp + 24], 3
     jz .ring0
 
     swapgs
 .ring0:
     sub rsp, TF_REGS_OFF + (16 * 8)
+    .cfi_adjust_cfa_offset TF_REGS_OFF + (16 * 8)
 
     mov qword ptr [rsp + TF_R15_OFF], r15
+    .cfi_offset r15, - (TF_SIZE - TF_R15_OFF)
     mov qword ptr [rsp + TF_R14_OFF], r14
+    .cfi_offset r14, - (TF_SIZE - TF_R14_OFF)
     mov qword ptr [rsp + TF_R13_OFF], r13
+    .cfi_offset r13, - (TF_SIZE - TF_R13_OFF)
     mov qword ptr [rsp + TF_R12_OFF], r12
+    .cfi_offset r12, - (TF_SIZE - TF_R12_OFF)
     mov qword ptr [rsp + TF_R11_OFF], r11
+    .cfi_offset r11, - (TF_SIZE - TF_R11_OFF)
     mov qword ptr [rsp + TF_R10_OFF], r10
+    .cfi_offset r10, - (TF_SIZE - TF_R10_OFF)
     mov qword ptr [rsp + TF_R9_OFF], r9
+    .cfi_offset r9, - (TF_SIZE - TF_R9_OFF)
     mov qword ptr [rsp + TF_R8_OFF], r8
+    .cfi_offset r8, - (TF_SIZE - TF_R8_OFF)
     mov qword ptr [rsp + TF_RAX_OFF], rax
+    .cfi_offset rax, - (TF_SIZE - TF_RAX_OFF)
     mov qword ptr [rsp + TF_RBX_OFF], rbx
+    .cfi_offset rbx, - (TF_SIZE - TF_RBX_OFF)
     mov qword ptr [rsp + TF_RCX_OFF], rcx
+    .cfi_offset rcx, - (TF_SIZE - TF_RCX_OFF)
     mov qword ptr [rsp + TF_RDX_OFF], rdx
+    .cfi_offset rdx, - (TF_SIZE - TF_RDX_OFF)
     mov qword ptr [rsp + TF_RSI_OFF], rsi
+    .cfi_offset rsi, - (TF_SIZE - TF_RSI_OFF)
     mov qword ptr [rsp + TF_RDI_OFF], rdi
+    .cfi_offset rdi, - (TF_SIZE - TF_RDI_OFF)
     mov qword ptr [rsp + TF_RBP_OFF], rbp
+    .cfi_offset rbp, - (TF_SIZE - TF_RBP_OFF)
 
     lea rax, [rsp + TF_VEC_OFF]
     mov qword ptr [rsp + TF_RSP_OFF], rax
@@ -122,6 +143,8 @@ IsrCommonInterruptStub:
     swapgs
 .return_ring0:
     iretq
+
+    .cfi_endproc
 .size IsrCommonInterruptStub, .-IsrCommonInterruptStub
 
 .section .text
@@ -131,14 +154,23 @@ IsrCommonInterruptStub:
     .set V, V+1
     .p2align 4, 0x90
     .globl IsrStub_\@
+    .type IsrStub_\@, @function
 IsrStub_\@:
+    .cfi_startproc simple
+    .cfi_signal_frame
+    .cfi_def_cfa rsp, 40
+
     .if !((V)==8 || (V)==10 || (V)==11 || (V)==12 || \
           (V)==13 || (V)==14 || (V)==17 || (V)==21 || (V)==29 || (V)==30)
         push 0
+        .cfi_adjust_cfa_offset 8
     .endif
 
     push V
+    .cfi_adjust_cfa_offset 8
     jmp IsrCommonInterruptStub
+
+    .cfi_endproc
     .size IsrStub_\@, .-IsrStub_\@
 
     .pushsection .data.isr_table, "aw"
--- supervisor/source/arch/x86_64/SystemStartup.asm
+++ supervisor/source/arch/x86_64/SystemStartup.asm
@@ -1,24 +1,38 @@
+.intel_syntax noprefix
+
 .section .text, "ax", @progbits
 .balign 16
 
 .extern Exec_InitializeEarly
 
 .global Supervisor_SystemStartup
-.global Exec_HaltCatchFire
-
+.type Supervisor_SystemStartup, @function
 Supervisor_SystemStartup:
+    .cfi_startproc
+    .cfi_undefined rip
+
     call Exec_InitializeEarly
     call Core_StackGuardInit
 
 #ifdef CI_BUILD
     call CiTest_Entry
 
-    mov $0xf4, %dx
-    out %al, %dx
+    mov dx, 0xf4
+    out dx, al
 #endif
 
+    .cfi_endproc
+.size Supervisor_SystemStartup, . - Supervisor_SystemStartup
+
+.global Exec_HaltCatchFire
+.type Exec_HaltCatchFire, @function
 Exec_HaltCatchFire:
+    .cfi_startproc
+
     cli
 1:
     hlt
     jmp 1b
+
+    .cfi_endproc
+.size Exec_HaltCatchFire, . - Exec_HaltCatchFire
