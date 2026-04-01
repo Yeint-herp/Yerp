ec6d8c7f39dd157d2eddab2d23cb4e2ef0beb6ef
Yeint Herp <yeint_herp@proton.me>
2026-04-01 09:35:57 +0200

    feat: Cpu sleep helper

--- supervisor/source/arch/CpuHint.h
+++ supervisor/source/arch/CpuHint.h
@@ -2,5 +2,6 @@
 #define SUPERVISOR_ARCH_CPUHINT_H
 
 void Arch_CpuRelax(void);
+void Arch_CpuSleep(void);
 
 #endif /* SUPERVISOR_ARCH_CPUHINT_H */
--- supervisor/source/arch/x86_64/CpuHint.c
+++ supervisor/source/arch/x86_64/CpuHint.c
@@ -4,3 +4,8 @@ void Arch_CpuRelax(void)
 {
     __asm__ volatile("pause" ::: "memory");
 }
+
+void Arch_CpuSleep(void)
+{
+    __asm__ volatile("hlt" ::: "memory");
+}
